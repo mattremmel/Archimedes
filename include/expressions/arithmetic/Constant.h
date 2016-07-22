64d186e45caba40602cb046d82b39ec822933d64
Matthew Remmel <matt.remmel@gmail.com>
2016-07-22 18:27:07 -0400

    Added Constant expression header

---
+++ include/expressions/arithmetic/Constant.h
@@ -0,0 +1,31 @@
+//
+// Constant.h
+// Archimedes
+//
+// Created by Matthew Remmel on 6/5/15.
+// Copyright (c) 2015 Matthew Remmel. All rights reserved.
+//
+
+#ifndef ARCHIMEDES_CONSTANT_H
+#define ARCHIMEDES_CONSTANT_H
+
+#include "Expression.h"
+
+namespace Archimedes {
+
+    class Constant : public Expression {
+    private:
+        double value;
+
+    public:
+        // Constructors
+        Constant(double value);
+
+        // Functions
+        double evaluate() override;
+        ExpressionType* getType() override;
+        std::string print() override;
+    };
+}
+
+#endif //ARCHIMEDES_CONSTANT_H
