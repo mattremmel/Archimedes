95db19ea899067d346b752377054effa559a2964
Matthew Remmel <matt.remmel@gmail.com>
2016-07-22 16:28:11 -0400

    Added Add expression header

---
+++ include/expressions/arithmetic/Add.h
@@ -0,0 +1,28 @@
+//
+// Add.h
+// Archimedes
+//
+// Created by Matthew Remmel on 5/11/15.
+// Copyright (c) 2015 Matthew Remmel. All rights reserved.
+//
+
+#ifndef ARCHIMEDES_ADD_H
+#define ARCHIMEDES_ADD_H
+
+#include "BinaryExpression.h"
+
+namespace Archimedes {
+
+    class Add : public BinaryExpression {
+    public:
+        // Constructors
+        Add(Expression* lvalue, Expression* rvalue);
+
+        // Functions
+        double evaluate() override;
+        ExpressionType getType() override;
+        std::string print() override;
+    };
+}
+
+#endif //ARCHIMEDES_ADD_H
