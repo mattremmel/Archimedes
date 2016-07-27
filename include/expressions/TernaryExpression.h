edd5be05e16797cdac35450ef9c561e3552d1927
Matthew Remmel <matt.remmel@gmail.com>
2016-07-27 10:45:05 -0400

    Added TernaryExpression header

---
+++ include/expressions/TernaryExpression.h
@@ -0,0 +1,28 @@
+//
+// TernaryExpression.h
+// Archimedes
+//
+// Created by Matthew Remmel on 7/27/16.
+// Copyright (c) 2015 Matthew Remmel. All rights reserved.
+//
+
+#ifndef ARCHIMEDES_TERNARYEXPRESSION_H
+#define ARCHIMEDES_TERNARYEXPRESSION_H
+
+#include "Expression.h"
+
+namespace Archimedes {
+
+    class TernaryExpression : public Expression {
+    protected:
+        Expression* value1;
+        Expression* value2;
+        Expression* value3;
+
+    public:
+        // Destructor
+        ~TernaryExpression() { delete(value); }
+    };
+}
+
+#endif //ARCHIMEDES_TERNARYEXPRESSION_H
