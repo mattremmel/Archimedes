930961d010d84ae94f1d811834a4df03b41f59cd
Matthew Remmel <matt.remmel@gmail.com>
2016-07-27 10:41:39 -0400

    Added GreaterThanOrEqual expression header

---
+++ include/expressions/boolean/GreaterThanOrEqual.h
@@ -0,0 +1,27 @@
+//
+// GreaterThanOrEqual.h
+// Archimedes
+//
+// Created by Matthew Remmel on 5/16/15.
+// Copyright (c) 2015 Matthew Remmel. All rights reserved.
+//
+
+#ifndef ARCHIMEDES_GREATERTHANOREQUAL_H
+#define ARCHIMEDES_GREATERTHANOREQUAL_H
+
+#include "BinaryExpression.h"
+
+namespace Archimedes {
+
+    class GreaterThanOrEqual : public BinaryExpression {
+        // Constructors
+        GreaterThanOrEqual(Expression* lvalue, Expression* rvalue);
+
+        // Functions
+        double evaluate() override;
+        ExpressionType getType() override;
+        std::string toString() override;
+    };
+}
+
+#endif //ARCHIMEDES_GREATERTHANOREQUAL_H
