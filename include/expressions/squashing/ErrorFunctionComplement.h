de61287905ae69852f8ee75ecfff8fea9295c865
Matthew Remmel <matt.remmel@gmail.com>
2016-07-29 23:10:00 -0400

    Added ErrorFunctionCompliment expression header

---
+++ include/expressions/squashing/ErrorFunctionComplement.h
@@ -0,0 +1,29 @@
+//
+// ErrorFunctionComplement.h
+// Archimedes
+//
+// Created by Matthew Remmel on 6/30/15.
+// Copyright (c) 2015 Matthew Remmel. All rights reserved.
+//
+
+#ifndef ARCHIMEDES_ERRORFUNCTIONCOMPLEMENT_H
+#define ARCHIMEDES_ERRORFUNCTIONCOMPLEMENT_H
+
+#include "UnaryExpression.h"
+
+namespace Archimedes {
+
+    class ErrorFunctionComplement : public UnaryExpression {
+    public:
+        // Constructors
+        ErrorFunctionComplement(Expression* value);
+
+        // Functions
+
+        double evaluate() override;
+        ExpressionsType getType() override;
+        std::string toString() override;
+    };
+}
+
+#endif //ARCHIMEDES_ERRORFUNCTIONCOMPLEMENT_H
