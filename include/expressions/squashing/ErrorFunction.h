6376d8e5fcc9dde52c67b06989518e4dcb36a971
Matthew Remmel <matt.remmel@gmail.com>
2016-07-29 23:08:35 -0400

    Added ErrorFunction expression header

---
+++ include/expressions/squashing/ErrorFunction.h
@@ -0,0 +1,27 @@
+//
+// ErrorFunction.h
+// Archimedes
+//
+// Created by Matthew Remmel on 6/30/15.
+// Copyright (c) 2015 Matthew Remmel. All rights reserved.
+//
+#ifndef ARCHIMEDES_ERRORFUNCTION_H
+#define ARCHIMEDES_ERRORFUNCTION_H
+
+#include "UnaryExpression.h"
+
+namespace Archimedes {
+
+    class ErrorFunction : public UnaryExpression {
+    public:
+        // Constructors
+        ErrorFunction(Expression* value);
+
+        // Functions
+        double evaluate() override;
+        ExpressionsType getType() override;
+        std::string toString() override;
+    };
+}
+
+#endif //ARCHIMEDES_ERRORFUNCTION_H
