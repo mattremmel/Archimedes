2c68788321a03d8ff061161fbdf7740c02b66cac
Matthew Remmel <matt.remmel@gmail.com>
2016-07-29 23:14:32 -0400

    Added LogisticFunction expression header

---
+++ include/expressions/squashing/LogisticFunction.h
@@ -0,0 +1,28 @@
+//
+// LogisticFunction.h
+// Archimedes
+//
+// Created by Matthew Remmel on 6/30/15.
+// Copyright (c) 2015 Matthew Remmel. All rights reserved.
+//
+
+#ifndef ARCHIMEDES_LOGISTICFUNCTION_H
+#define ARCHIMEDES_LOGISTICFUNCTION_H
+
+#include "UnaryExpression.h"
+
+namespace Archimedes {
+
+    class LogisticFunction : public UnaryExpression {
+    public:
+        // Constructors
+        LogisticFunction(Expression* value);
+
+        // Functions
+        double evaluate() override;
+        ExpressionsType getType() override;
+        std::string toString() override;
+    };
+}
+
+#endif //ARCHIMEDES_LOGISTICFUNCTION_H
