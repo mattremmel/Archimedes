72ba755be4f3f05fc3a492042864192a97e65fac
Matthew Remmel <matt.remmel@gmail.com>
2016-07-29 23:11:32 -0400

    Added GaussianFunction expression header

---
+++ include/expressions/squashing/GaussianFunction.h
@@ -0,0 +1,28 @@
+//
+// GaussianFunction.h
+// Archimedes
+//
+// Created by Matthew Remmel on 6/30/15.
+// Copyright (c) 2015 Matthew Remmel. All rights reserved.
+//
+
+#ifndef ARCHIMEDES_GAUSSIANFUNCTION_H
+#define ARCHIMEDES_GAUSSIANFUNCTION_H
+
+#include "UnaryExpression.h"
+
+namespace Archimedes {
+
+    class GaussianFunction : public UnaryExpression {
+    public:
+        // Constructors
+        GaussianFunction(Expression* value);
+
+        // Functions
+        double evaluate() override;
+        ExpressionsType getType() override;
+        std::string toString() override;
+    };
+}
+
+#endif //ARCHIMEDES_GAUSSIANFUNCTION_H
